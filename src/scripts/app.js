a2c395e0856d7a0fc8339d8c7423b6ac2b51bb23
Salwa ★ <loizoo.dwt@gmail.com>
2024-04-26 13:39:53 +0200

    paint

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,87 @@
-console.info('Hello world');
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const clearBtn = document.getElementById('clearBtn');
+const colorPicker = document.getElementById('colorPicker');
+const sizeSlider = document.getElementById('sizeSlider');
+const eraserCheckbox = document.getElementById('eraserCheckbox');
+
+
+canvas.width = 400; // Remplacez 800 par la largeur souhaitée en pixels
+canvas.height = 400;
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let strokeColor = colorPicker.value;
+let strokeWidth = sizeSlider.value;
+let isEraserEnabled = false;
+
+
+function startDrawing(e) {
+  isDrawing = true;
+  lastX = e.clientX - canvas.offsetLeft;
+  lastY = e.clientY - canvas.offsetTop;
+}
+
+
+function draw(e) {
+  if (!isDrawing) return;
+  
+  const x = e.clientX - canvas.offsetLeft;
+  const y = e.clientY - canvas.offsetTop;
+  
+  ctx.beginPath();
+  ctx.moveTo(lastX, lastY);
+  ctx.lineTo(x, y);
+  ctx.strokeStyle = isEraserEnabled ? 'white' : strokeColor;
+  ctx.lineWidth = strokeWidth;
+  ctx.stroke();
+  
+  lastX = x;
+  lastY = y;
+}
+
+// Функція для зупинки малювання
+function stopDrawing() {
+  isDrawing = false;
+}
+
+// Очистка полотна
+function clearCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+// Оновлення кольору пензля
+function updateColor() {
+  strokeColor = colorPicker.value;
+}
+
+// Оновлення розміру пензля
+function updateSize() {
+  strokeWidth = sizeSlider.value;
+}
+
+// Оновлення стану гумки
+function updateEraser() {
+  isEraserEnabled = eraserCheckbox.checked;
+}
+
+// Додаємо обробники подій для малювання
+canvas.addEventListener('mousedown', startDrawing);
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', stopDrawing);
+canvas.addEventListener('mouseout', stopDrawing);
+
+// Додаємо обробник події для кнопки очищення
+clearBtn.addEventListener('click', clearCanvas);
+
+// Додаємо обробник події для вибору кольору
+colorPicker.addEventListener('change', updateColor);
+
+// Додаємо обробник події для зміни розміру
+sizeSlider.addEventListener('input', updateSize);
+
+// Додаємо обробник події для вибору гумки
+eraserCheckbox.addEventListener('change', updateEraser);
+
