314ae322af4ecf293a29eae0c6c8bf5c04f39f22
Salwa ★ <loizoo.dwt@gmail.com>
2024-05-01 10:18:55 +0200

    pseudo

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,21 @@
-console.info('Hello world');
+
+/////pseudooooo
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+ 
+  // Charger le pseudo enregistré lorsque la page est chargée
+  var pseudoEnregistre = localStorage.getItem("pseudo");
+  if (pseudoEnregistre) {
+    document.getElementById("pseudoAffiche").innerText = pseudoEnregistre;
+  }
+});
+document.querySelector('.bouton__enregistrer').addEventListener('click', function () {
+    sauvegarderPseudo();
+})
+function sauvegarderPseudo() {
+    var pseudo = document.getElementById("pseudoInput").value;
+    localStorage.setItem("pseudo", pseudo);
+    document.getElementById("pseudoAffiche").innerText = pseudo;
+  }
