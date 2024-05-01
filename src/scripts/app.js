30c9b217d0f552e9c11c696a994ed5a20b57a193
Salwa ★ <loizoo.dwt@gmail.com>
2024-05-01 10:11:16 +0200

    nav card

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,13 @@
-console.info('Hello world');
+
+"use strict";
+
+document.addEventListener('DOMContentLoaded', function () {
+    const burger = document.querySelector('.navbar__burger');
+    const nav = document.querySelector('.navbar__nav');
+  
+    burger.addEventListener('click', function () {
+      burger.classList.toggle('active');
+      nav.classList.toggle('active');
+    });
+  });
+  
